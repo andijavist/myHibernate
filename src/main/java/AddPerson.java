910bc2b29782f4907368a08176f7e87ad7386899
andijavist <grungeisnotdead1@yandex.ru>
2021-04-23 13:00:51 +0300

    поправил косяк сохранения

--- src/main/java/AddPerson.java
+++ src/main/java/AddPerson.java
@@ -1,35 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.*;
 
 public class AddPerson {
 
-        //первую строку просто надо запомнить или содрать тут https://jdbc.postgresql.org/documentation/head/connect.html
-        //препод вместо localhost почему то пишет 127.0.0.1 (коненчо одно и то же, но почему то именно IP, а не DNS)
         private static final String URL = "jdbc:postgresql://127.0.0.1:5432/postgres";
         private static final String LOGIN = "postgres";
         private static final String PASSWORD = "postgres";
-        protected void doAdd(String name, String surname) {
-            //ОЧЕНЬ ОБЯЗАТЕЛЬНАЯ ТЕМА ДЯЛ ЗАГРУЗЧИКА КЛАССОВ!!!!!!!!!!!!!!!!
+
+    public static void main(String[] args) {
+
             try {
+                BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
                 Class.forName("org.postgresql.Driver");
+
                 Connection connection = DriverManager.getConnection(URL,LOGIN,PASSWORD);
-                PreparedStatement statement = connection.prepareStatement("insert into users " +
+                PreparedStatement statement = connection.prepareStatement
+                        ("insert into users (first_name, last_name) values (?, ?)");
 
-                        "(first_name, last_name) values (?, ?)");
                 System.out.println("Enter author name:");
-                //TODO
-                String param = scanner.nextLine();
-
-                statement.setString(1, param);
+                String name = read.readLine();
+                statement.setString(1, name);
 
                 System.out.println("Enter author last name:");
-                param = scanner.nextLine();
-                statement.setString(2, param);
+                String lastName = read.readLine();
+                statement.setString(2, lastName);
 
                 statement.executeUpdate();
 
-            } catch (ClassNotFoundException | SQLException e) {
+            } catch (ClassNotFoundException | SQLException | IOException e) {
                 e.printStackTrace();
             }
+    }
+        }
 
 
         }
